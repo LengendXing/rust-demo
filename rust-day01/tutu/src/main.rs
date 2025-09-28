26f98112b4ffb49bdd6199210af84ab5f20af0c5
LengendXing <126065917+LengendXing@users.noreply.github.com>
2025-09-28 08:03:54 +0000

    1

--- rust-day01/tutu/src/main.rs
+++ rust-day01/tutu/src/main.rs
@@ -1,11 +1,3 @@
-
-/// #[rust Application]
-fn main() {
-    println!("Hello, world!");
-    do_const_dif();
-}
-
-
 /// Test function for variable declarations and assignments
 /// This function does not return any value and is used to verify
 /// that variables can be declared and assigned correctly.
@@ -33,4 +25,43 @@ fn do_const_dif() {
      // defined c as boolean
     let c:bool = true;  
     println!("a: {}, b: {}, c: {}", a, b, c);
+}
+
+fn write_rust_as_java_script() {
+    // defined a as integer
+    let a;
+    // defined b as integer and assigned value 1
+    let b = 1;
+    // defined a as boolean
+    a = false;
+     // defined c as boolean
+    let c:bool = true;  
+    println!("a: {}, b: {}, c: {}", a, b, c);
+}
+
+fn test_muti_define() {
+    let mut x = 1;
+    x += 1;
+    println!("x: {}", x);
+}
+
+fn test_static_var() {
+    static VAR: i32 = 100;
+    static N:i32 = 200;
+    static mut M:i32 = 300;
+    println!("VAR: {}", VAR);
+    println!("N: {}", N);
+    unsafe {
+        M += 1;
+    }
+    println!("M: {}", unsafe { M });
+}
+
+/// #[rust Application]
+fn main() {
+    println!("Hello, world!");
+    do_const_dif();
+    write_rust_as_java_script();
+    test_muti_define();
+    test_static_var();
 }
